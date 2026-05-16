f9ddae591b347eac2e95021e434deaef3ccfb00a
KidiXDev <dikhiardian55@gmail.com>
2026-05-16 16:19:27 +0700

    feat: crash log

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/kidixdev/lofi-radio/internal/config"
+	"github.com/kidixdev/lofi-radio/internal/radio"
 	"github.com/kidixdev/lofi-radio/internal/tui"
 	"os"
 )
 
 func main() {
+	radio.InitCrashLogging()
+
+	defer func() {
+		if recovered := recover(); recovered != nil {
+			radio.Logf("main.panic recovered=%v", recovered)
+			fmt.Println("Panic:", recovered)
+			os.Exit(1)
+		}
+	}()
+
 	if err := tui.Run(config.PlaylistURL); err != nil {
+		radio.Logf("main.error err=%v", err)
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
